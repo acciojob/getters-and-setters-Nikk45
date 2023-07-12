296e54072d36187834293f3a41c97e8a612016bd
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-12 18:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ class Teacher extends Person {
 	}
 }
 
+const person = new Person('John',25);
+const student = new Student("John", 30);
+const teacher = new Teacher("John", 30);
+
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
