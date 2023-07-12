df7f7c81ab2bdfe87c344b4a456aa6d1b029715c
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-07-12 19:05:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,17 +15,58 @@ class Person {
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`);
-	}
+// 	study(){
+// 		console.log(`${this.name} is studying`);
+// 	}
+// }
+
+// class Teacher extends Person {
+// 	teach(){
+// 		console.log(`${this.name} is teaching`);
+// 	}
+// }
+
+// const person = new Person('John', 25);
+// console.log(person.name);  // Output: John
+// person.age = 30;
+// console.log(person.age);  // Output: 30
+
+// const student = new Student('Alice', 20);
+// console.log(student.name);  // Output: Alice
+// student.study();            // Output: Alice is studying.
+
+// const teacher = new Teacher('Bob', 35);
+// console.log(teacher.name);  // Output: Bob
+// teacher.teach(); 
+
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
 }
 
+// Creating instances and using the classes
 const person = new Person('John', 25);
 console.log(person.name);  // Output: John
 person.age = 30;
@@ -38,7 +79,7 @@ student.study();            // Output: Alice is studying.
 const teacher = new Teacher('Bob', 35);
 console.log(teacher.name);  // Output: Bob
 teacher.teach(); 
-
+	
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
